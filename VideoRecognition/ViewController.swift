e89bf9e16b1d629e2f9a90a5cc4bc090a98ff8e4
syj <ssyyjj888@126.com>
2017-07-21 19:31:06 +0800

    字幕

--- VideoRecognition/ViewController.swift
+++ VideoRecognition/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 import Speech
+import AVFoundation
 
 class ViewController: UIViewController {
 
     //初始化一个识别器
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
-//    private var tap: MYAudioTapProcessor!
+    public  var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
     private var player = AVQueuePlayer()
     private let playerLayer = AVPlayerLayer()
+    private var tap: MYAudioTapProcessor!
     
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var playerView: UIView!
@@ -30,25 +34,25 @@ class ViewController: UIViewController {
             assert(status == .authorized)
             DispatchQueue.main.async {
                 self.setupVideo()
-//                self.setupRecognition()
+                self.setupRecognition()
             }
             
-            //path
-            let url = Bundle.main.url(forResource: "video", withExtension: "mp4")
-            
-            //初始化一个识别请求
-            let request = SFSpeechURLRecognitionRequest(url: url!)
-            
-            //开始一个识别请求
-            self.speechRecognizer.recognitionTask(with: request, resultHandler: { (result, error) in
-                if error != nil{
-                    print("识别错误")
-                    return
-                }
-                else{
-                    print(result?.bestTranscription.formattedString)
-                }
-            })
+//            //path
+//            let url = Bundle.main.url(forResource: "video", withExtension: "mp4")
+//            
+//            //初始化一个识别请求
+//            let request = SFSpeechURLRecognitionRequest(url: url!)
+//            
+//            //开始一个识别请求
+//            self.speechRecognizer.recognitionTask(with: request, resultHandler: { (result, error) in
+//                if error != nil{
+//                    print("识别错误")
+//                    return
+//                }
+//                else{
+//                    print(result?.bestTranscription.formattedString)
+//                }
+//            })
         }
     }
     
@@ -64,24 +68,54 @@ class ViewController: UIViewController {
         // great thanks to AVFoundation, CoreFoundation and SpeechKit engineers for helping to figure this out!
         // especially to Eric Lee for explaining how to convert AudioBufferList -> AVAudioPCMBuffer
         
-//        tap = MYAudioTapProcessor(audioAssetTrack: audioTrack)
-//        tap.delegate = self
+        tap = MYAudioTapProcessor(audioAssetTrack: audioTrack)
+        tap.delegate = self
         
         // Video playback
         let item = AVPlayerItem(asset: asset)
         player.insert(item, after: nil)
         player.play()
-//        player.currentItem?.audioMix = tap.audioMix
+        player.currentItem?.audioMix = tap.audioMix
         playerLayer.player = player
         playerLayer.frame = playerView.bounds;
         playerView.layer.insertSublayer(playerLayer, at: 0)
+    }
+    
+    private func setupRecognition() {
+        let recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        // we want to get continuous recognition and not everything at once at the end of the video
+        recognitionRequest.shouldReportPartialResults = true
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { [unowned self] result, error in
+            
+            self.textView.text = result?.bestTranscription.formattedString
+            print("text:", result?.bestTranscription.formattedString);
+            // once in about every minute recognition task finishes so we need to set up a new one to continue recognition
+            if result?.isFinal == true {
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.setupRecognition()
+            }
+        }
+        self.recognitionRequest = recognitionRequest
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension ViewController: MYAudioTabProcessorDelegate {
+    func audioTabProcessor(_ audioTabProcessor: MYAudioTapProcessor!, hasNewLeftChannelValue leftChannelValue: Float, rightChannelValue: Float) {
+//        recognitionRequest?.append(buffer)
+    }
 
 
+    // getting audio buffer back from the tap and feeding into speech recognizer
+    @available(iOS 8.0, *)
+    func audioTabProcessor(_ audioTabProcessor: MYAudioTapProcessor!, didReceive buffer: AVAudioPCMBuffer!) {
+        recognitionRequest?.append(buffer)
+    }
 }
 
